ac3e48fc4317d062c5750b4afcb220c91a9a8b83
prototype2010 <Firstday2010>
2020-10-26 12:13:46 +0200

    db connection successful

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {TypeOrmModule} from "@nestjs/typeorm";
+
+import { db} from 'config';
+
+const {type, port, database, synchronize, username, password} = db
 
 @Module({
-  imports: [],
+  imports: [
+      TypeOrmModule.forRoot({
+        type,
+        host: 'localhost',
+        port,
+        username,
+        password,
+        database,
+        entities: [],
+        synchronize,
+      })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
