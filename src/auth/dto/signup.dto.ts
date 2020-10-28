a9a143207fb1dc128548df7dbb285a3dedb13bc6
prototype2010 <Firstday2010>
2020-10-28 18:00:20 +0200

    Joi validation

--- src/auth/dto/signup.dto.ts
+++ src/auth/dto/signup.dto.ts
@@ -1,6 +1,9 @@
 import { IsEmail, IsString } from 'class-validator';
 import { PUBLIC_USER_ROLES } from '../../entity/user.entity';
 
+import * as Joi from '@hapi/joi';
+import { EXCEPTION_MESSAGES } from '../../common/exceptionMessages';
+
 export class SignUpDTO {
   @IsEmail()
   email: string;
@@ -23,3 +26,26 @@ export class SignUpDTO {
   @IsString()
   lastName: string;
 }
+
+export const SignUpSchema = Joi.object().keys({
+  email: Joi.string()
+    .email()
+    .required(),
+  phone: Joi.string()
+    .min(6)
+    .max(20)
+    .required(),
+  password: Joi.string()
+    .min(8)
+    .required()
+    .messages({
+      'any.only': EXCEPTION_MESSAGES.PASSWORDS_SHOULD_MATCH,
+    }),
+  passwordConfirmation: Joi.string()
+    .valid(Joi.ref('password'))
+    .required(),
+
+  role: Joi.string()
+    .valid(...Object.values(PUBLIC_USER_ROLES))
+    .required(),
+});
---
+++ src/common/exceptionMessages.ts
@@ -0,0 +1,3 @@
+export enum EXCEPTION_MESSAGES {
+  PASSWORDS_SHOULD_MATCH = 'Passwords should match',
+}
