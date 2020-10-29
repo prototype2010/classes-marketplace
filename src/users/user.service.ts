55bfd7d73a47e59a5db4cd5dbd1286a2ad63295d
prototype2010 <Firstday2010>
2020-10-29 17:16:57 +0200

    repository thing

--- src/users/user.service.ts
+++ src/users/user.service.ts
@@ -1,16 +1,11 @@
 import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
-import { Connection, Repository } from 'typeorm';
+import { Connection } from 'typeorm';
 import { SignUpDTO } from '../auth/dto/signup.dto';
 
 @Injectable()
 export class UserService {
   private userRepository: UserRepository;
-  // constructor(
-  //   @InjectRepository(UserRepository)
-  //   private readonly repo: Repository<User>,
-  // ) {}
   constructor(private readonly connection: Connection) {
     this.userRepository = this.connection.getCustomRepository<UserRepository>(
       UserRepository,
@@ -18,13 +13,6 @@ export class UserService {
   }
 
   async createUser(signUpDTO: SignUpDTO) {
-    try {
-      return this.userRepository.createUser(signUpDTO as any).catch(e => {
-        var x = 1;
-      });
-    } catch (e) {
-      console.error('################################');
-      console.error(e);
-    }
+    return this.userRepository.createUser(signUpDTO as any);
   }
 }
